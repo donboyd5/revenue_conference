60e9090b63b2ba37860fc1d4206df64c04de9890
Don Boyd <donboyd5@gmail.com>
2026-02-02 10:48:43 -0500

    initial files for qcew

---
+++ libraries_ts.R
@@ -0,0 +1,15 @@
+# https://robjhyndman.com/software/
+
+library(forecast)
+library(tsibble)
+library(feasts)
+library(fable)
+library(fabletools)
+library(RcppRoll)
+library(slider)
+
+library(broom)
+# library(tidyverts)
+library(tsibbledata)
+
+library(fable.prophet)
