f4f220cd06939bab47b7ea2f8c79fa628b2635ec
Don Boyd <donboyd5@gmail.com>
2026-02-06 14:13:48 -0500

    WIP

--- setup.R
+++ setup.R
@@ -7,6 +7,7 @@ DBEA <- fs::path("D:/", "data", "bea") # bea data
 DCAPGAINS <- fs::path("D:/", "data", "capgains") # capital gains data
 DCES <- fs::path("D:/", "data", "ces") # current employment statistics
 DCESNY <- fs::path(DCES, "cesny") # current employment statistics ny state
+DFEDNY <- fs::path("D:/", "data", "nyfed") # federal reserve bank of new york data
 DPITNY <- fs::path("D:/", "data", "nyspit") # current employment statistics
 DQCEW <- fs::path("D:/", "data", "qcew")
 
